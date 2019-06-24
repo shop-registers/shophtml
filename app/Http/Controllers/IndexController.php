8955cedf1a458c736e58891613477ea476ae20da
EngineerGitZhang <625811388@qq.com>
2019-06-24 08:35:27 +0800

    'add'

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -12,34 +12,45 @@ class IndexController extends Controller
     {
     	// 无限分类
     	$type = Cache::remember('type',120,function(){
-    		$str = file_get_contents('http://localhost/show_qian/public/goodstype');
+    		$str = file_get_contents('http://www.hhh.com/api/goodstype');
     		return json_decode($str,true);
     	});
 
     	//分类商品1
     	$data1 = Cache::remember('data1',120,function(){
-    		$str = file_get_contents('http://localhost/show_qian/public/getgoods1');
+    		$str = file_get_contents('http://www.hhh.com/api/getgoods1');
     		return json_decode($str,true);
     	});
 
     	//分类商品2
     	$data2 = Cache::remember('data2',120,function(){
-    		$str = file_get_contents('http://localhost/show_qian/public/getgoods2');
+    		$str = file_get_contents('http://www.hhh.com/api/getgoods2');
     		return json_decode($str,true);
     	});
 
     	//分类商品3
     	$data3 = Cache::remember('data3',120,function(){
-    		$str = file_get_contents('http://localhost/show_qian/public/getgoods3');
+    		$str = file_get_contents('http://www.hhh.com/api/getgoods3');
     		return json_decode($str,true);
     	});
 
     	//分类商品4
     	$data4 = Cache::remember('data4',120,function(){
-    		$str = file_get_contents('http://localhost/show_qian/public/getgoods4');
+    		$str = file_get_contents('http://www.hhh.com/api/getgoods4');
     		return json_decode($str,true);
     	});
-    	//print_r($data1);die;
-    	return view('index',['data'=>$type,'data1'=>$data1,'data2'=>$data2,'data3'=>$data3,'data4'=>$data4]);
+
+        //图片
+        $img = Cache::remember('img',120,function(){
+            $str = file_get_contents('http://www.hhh.com/api/brand');
+            return json_decode($str,true);
+        });
+    	//print_r($img);die;
+    	return view('index',['data'=>$type,'data1'=>$data1,'data2'=>$data2,'data3'=>$data3,'data4'=>$data4,'img'=>$img]);
+    }
+
+    public function spike(Request $request,$id=null)
+    {
+        return view('spike');
     }
 }
---
+++ resources/views/header.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+		<title>搜索页面</title>
+
+		<link href="static/AmazeUI-2.4.2/assets/css/amazeui.css" rel="stylesheet" type="text/css" />
+		<link href="static/AmazeUI-2.4.2/assets/css/admin.css" rel="stylesheet" type="text/css" />
+
+		<link href="static/basic/css/demo.css" rel="stylesheet" type="text/css" />
+
+		<link href="static/css/seastyle.css" rel="stylesheet" type="text/css" />
+
+		<script type="text/javascript" src="static/basic/js/jquery-1.7.min.js"></script>
+		<script type="text/javascript" src="static/js/script.js"></script>
+	</head>
+
+	<body>
+
+		<!--顶部导航条 -->
+		<div class="am-container header">
+			<ul class="message-l">
+				<div class="topMessage">
+					<div class="menu-hd">
+						<a href="#" target="_top" class="h">亲，请登录</a>
+						<a href="#" target="_top">免费注册</a>
+					</div>
+				</div>
+			</ul>
+			<ul class="message-r">
+				<div class="topMessage home">
+					<div class="menu-hd"><a href="#" target="_top" class="h">商城首页</a></div>
+				</div>
+				<div class="topMessage my-shangcheng">
+					<div class="menu-hd MyShangcheng"><a href="#" target="_top"><i class="am-icon-user am-icon-fw"></i>个人中心</a></div>
+				</div>
+				<div class="topMessage mini-cart">
+					<div class="menu-hd"><a id="mc-menu-hd" href="#" target="_top"><i class="am-icon-shopping-cart  am-icon-fw"></i><span>购物车</span><strong id="J_MiniCartNum" class="h">0</strong></a></div>
+				</div>
+				<div class="topMessage favorite">
+					<div class="menu-hd"><a href="#" target="_top"><i class="am-icon-heart am-icon-fw"></i><span>收藏夹</span></a></div>
+			</ul>
+			</div>
+
+			<!--悬浮搜索框-->
+
+			<div class="nav white">
+				<div class="logo"><img src="static/images/logo.png" /></div>
+				<div class="logoBig">
+					<li><img src="static/images/logobig.png" /></li>
+				</div>
+
+				<div class="search-bar pr">
+					<a name="index_none_header_sysc" href="#"></a>
+					<form>
+						<input id="searchInput" name="index_none_header_sysc" type="text" placeholder="搜索" autocomplete="off">
+						<input id="ai-topsearch" class="submit am-btn"  value="搜索" index="1" type="submit">
+					</form>
+				</div>
+			</div>
+
+			<div class="clear"></div>
+			<b class="line"></b>
+           <div class="search">
+			<div class="search-list">
+			<div class="nav-table">
+					   <div class="long-title"><span class="all-goods">全部分类</span></div>
+					   <div class="nav-cont">
+							<ul>
+								<li class="index"><a href="#">首页</a></li>
+                                <li class="qc"><a href="#">闪购</a></li>
+                                <li class="qc"><a href="#">限时抢</a></li>
+                                <li class="qc"><a href="#">团购</a></li>
+                                <li class="qc last"><a href="#">大包装</a></li>
+							</ul>
+						    <div class="nav-extra">
+						    	<i class="am-icon-user-secret am-icon-md nav-user"></i><b></b>我的福利
+						    	<i class="am-icon-angle-right" style="padding-left: 10px;"></i>
+						    </div>
+						</div>
+			</div>
+			@yield('content')
+			</div>
+
+	</body>
+
+</html>
